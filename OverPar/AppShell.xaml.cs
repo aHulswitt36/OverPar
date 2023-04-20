cfa0e79934a37073e68bec52080c08189a2023b8
Aarron Hulswitt <aarron.hulswitt@gmail.com>
2023-04-19 21:42:41 -0400

    Add project files.

---
+++ OverPar/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OverPar;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ OverPar/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace OverPar;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
