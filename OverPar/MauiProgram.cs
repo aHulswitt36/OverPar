17de76bc76c78c3b5aad59d779e68bee02d32616
Aarron Hulswitt <aarron.hulswitt@gmail.com>
2023-04-26 22:44:05 -0400

    trying to get the appsettings to work on maui so we can pass around the azure settings

--- OverPar/MauiProgram.cs
+++ OverPar/MauiProgram.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using OverPar.Pages;
 using OverPar.ViewModels;
+using System.Reflection;
+using OverPar.Domain.Settings;
 
 namespace OverPar;
 
@@ -16,6 +19,16 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+		//var a = Assembly.GetExecutingAssembly();
+		//using var stream = a.GetManifestResourceStream("OverPar.appsettings.json");
+		//var config = new ConfigurationBuilder()
+		//	.AddJsonStream(stream)
+		//	.Build();
+
+		//builder.Configuration.AddConfiguration(config);
+		builder.Configuration.AddJsonFile("OverPar.appsettings.json");
+		builder.Services.AddOptions();
+		builder.Services.Configure<FormReaderSettings>(builder.Configuration.GetRequiredSection("ScorecardReader"));
 
 		builder.Services.AddSingleton<MainViewModel>();
 		builder.Services.AddTransient<ScorecardViewModel>();
